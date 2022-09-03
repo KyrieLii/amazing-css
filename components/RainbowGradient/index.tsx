a8c08a2840dedbbcb12a50363c3eec2550f65437
limao2021 <limao2021@bytedance.com>
2022-09-03 18:01:50 +0800

    feat: init & RainbowGradient

---
+++ components/RainbowGradient/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./index.module.scss";
+
+const RainbowGradient = () => (
+  <div className={styles.outer}>
+    <div className={styles.wrapper}>
+      <div className={styles.rainbowGradient}></div>
+      <div className={styles.childWrapper}>
+        <button className={(styles.btn, styles.btn_reset)}>
+          Rainbow Gradient
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default RainbowGradient;
